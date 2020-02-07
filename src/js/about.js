26e4a00f955286cfc88656980bc7799a457c19d7
FurryCat <furrycat.web@gmail.com>
2020-02-07 16:13:17 +0300

    add linting

--- src/js/about.js
+++ src/js/about.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import Header from '~/js/components/header';
 
 if (true) {
-  console.log(2);
+  console.log(2)
 }
 else {
-  console.log("22");
+  console.log("22")
 
 }
 
